ffdf6b760c986023cfe508c77735c5d39ec66395
Kaamil12 <69597401+Kaamil12@users.noreply.github.com>
2021-02-28 18:30:28 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,17 +6,21 @@ var foodS
 
 function preload()
 {
-  addImage(dog,"dogImg.png");
-  addImage(happyDog,"dogImg1.png");
-  foodStock = database.ref('Food');
-  footStock.on('value', readStock);
+ dogImage= loadImage("images/dogImg.png")
+ happydogImage = loadImage("images/dogImg1.png")
 }
 
 function setup() {
 	createCanvas(500, 500);
   
   database = firebase.database();
+  dog = createSprite(250,250);
+
+  dog.addImage(dogImage);
+  dog.scale = 0.15;
   
+  foodStock = database.ref('Food');
+  foodStock.on('value', readStock);
 }
 
 
@@ -26,12 +30,15 @@ function draw() {
   
   if(keyWentDown(UP_ARROW)) {
     writeStock(foodS);
-    dog.addImage(dogImg1.png);
+    dog.addImage(happydogImage);
   }
   
   textSize(35);
   fill(15);
   stroke(25);
+  text("food remaining:" + foodS,170,200);
+  textSize(13);
+  text("Press up arrow key to feed the dog",130,10,300,10)
 }
 
 function readStock(data) {
@@ -40,7 +47,11 @@ function readStock(data) {
 }
 
 function writeStock(x) {
-
+if(x<=0){
+  x=0;
+}else{
+  x=x-1
+}
   database.ref('/').update ({
     Food:x
   })
